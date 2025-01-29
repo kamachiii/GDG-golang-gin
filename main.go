f508f15ec13e6b3a2051df08a61dc3927124512e
kamachiii <hengkert3jy@gmail.com>
2025-01-29 19:31:38 +0700

    add database setup and CRUD operations for albums

--- main.go
+++ main.go
@@ -1,86 +1,131 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type album struct {
+type Album struct {
 	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	Title  string  `json:"title" binding:"required"`
+	Price  float64 `json:"price" binding:"required"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+var db *sql.DB
+
+func InitDB(database string) error {
+	var err error
+	db, err = sql.Open("mysql", database)
+	if err != nil {
+		return err
+	}
+
+	if err = db.Ping(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	rows, err := db.Query("SELECT id, title, price FROM albums")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var albums []Album
+	for rows.Next() {
+		var album Album
+		if err := rows.Scan(&album.ID, &album.Title, &album.Price); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		albums = append(albums, album)
+	}
+	c.JSON(http.StatusOK, albums)
 }
 
 func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
-
-	for _, album := range albums {
-		if album.ID == id {
-			c.IndentedJSON(http.StatusOK, album)
-			return
-		}
-	}
-
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	if err := c.BindJSON(&newAlbum); err != nil {
+	var album Album
+	err := db.QueryRow("SELECT id, title, price FROM albums WHERE id = ?", id).Scan(&album.ID, &album.Title, &album.Price)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	c.JSON(http.StatusOK, album)
 }
 
-func putAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	for i, album := range albums {
-		if album.ID == id {
-			albums[i] = album
-			c.IndentedJSON(http.StatusOK, album)
-			return
-		}
+func addAlbum(c *gin.Context) {
+	var album Album
+	if err := c.ShouldBindJSON(&album); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
+	_, err := db.Exec("INSERT INTO albums (id, title, price) VALUES (?, ?, ?)", album.ID, album.Title, album.Price)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, album)
+}
 
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+func updateAlbum(c *gin.Context) {
+	id := c.Param("id")
+	var album Album
+	if err := c.ShouldBindJSON(&album); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	_, err := db.Exec("UPDATE albums SET title = ?, price = ? WHERE id = ?", album.Title, album.Price, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, album)
 }
 
 func deleteAlbum(c *gin.Context) {
 	id := c.Param("id")
-
-	for i, album := range albums {
-		if album.ID == id {
-			albums = append(albums[:i], albums[i+1:]...)
-			c.IndentedJSON(http.StatusOK, gin.H{"message": "album deleted"})
-			return
-		}
+	_, err := db.Exec("DELETE FROM albums WHERE id = ?", id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
-
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.JSON(http.StatusOK, gin.H{"message": "Album deleted"})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
-	router.PUT("/albums/:id", putAlbumByID)
-	router.DELETE("/albums/:id", deleteAlbum)
-	router.Run("localhost:8080")
+	host := "localhost"
+	port := "3306"
+	user := "root"
+	dbname := "gdg_backend_go_gin"
+	database := user + "@" + "tcp(" + host + ":" + port + ")/" + dbname
+	/*
+	 Jika database menggunakan password
+	 	password := "your_password"
+	 	database := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbname
+	*/
+
+	if err := InitDB(database); err != nil {
+		router.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		})
+	} else {
+		router.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"message": "DB connected"})
+		})
+		router.GET("/albums", getAlbums)
+		router.GET("/albums/:id", getAlbumByID)
+		router.POST("/albums", addAlbum)
+		router.PUT("/albums/:id", updateAlbum)
+		router.DELETE("/albums/:id", deleteAlbum)
+	}
+
+	router.Run(":8080")
 }
---
+++ readme.md
@@ -0,0 +1,71 @@
+# Gin Backend
+
+Ini adalah proyek latihan untuk membuat aplikasi CRUD menggunakan framework Gin dengan database MySQL.
+
+## Instalasi
+
+1. Clone repositori ini:
+    ```bash
+    git clone https://github.com/username/repo.git
+    ```
+2. Masuk ke direktori proyek:
+    ```bash
+    cd repo
+    ```
+3. Instal dependensi:
+    ```bash
+    go mod tidy
+    ```
+
+## Menjalankan Aplikasi
+
+Untuk menjalankan aplikasi, gunakan perintah berikut:
+```bash
+go run main.go
+```
+
+Aplikasi akan berjalan di `http://localhost:8080`.
+
+## Struktur Proyek
+
+- `main.go`: File utama untuk menjalankan aplikasi.
+- `go.mod`: Berisi informasi tentang modul Go dan dependensi yang digunakan dalam proyek.
+- `go.sum`: Berisi checksum dari setiap dependensi yang digunakan untuk memastikan integritas.
+
+
+## Konfigurasi
+
+```json
+{
+    "port": "8080",
+    "database": {
+        "host": "localhost",
+        "port": "3306",
+        "user": "root",
+        "password": "",
+        "dbname": "gdg_backend_go_gin"
+    }
+}
+```
+
+## Rute API
+
+Berikut adalah beberapa rute API yang tersedia:
+- `GET /`: Mengecek status db connection
+- `GET /albums`: Mendapatkan semua data.
+- `GET /albums/:id`: Mendapatkan data berdasarkan id.
+- `POST /album`: Membuat data baru.
+- `PUT /album/:id`: Memperbarui data berdasarkan ID.
+- `DELETE /album/:id`: Menghapus data berdasarkan ID.
+
+## Format melakukan POST dan PUT
+
+Berikut adalah format raw untuk menambah dan mengupdate data:
+```json
+{
+    "id": "5",
+    "title": "Si Jabeng",
+    "price": 89.99
+}
+```
+
